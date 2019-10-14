5d93088682506d03fbcddcce2aa73d3856b1c89a
ZJam <670231925@qq.com>
2019-10-14 14:17:26 +0800

    Initial Commit

---
+++ VideoExporter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VideoExporter
+//
+//  Created by zjj on 2019/10/14.
+//  Copyright © 2019 zjj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
